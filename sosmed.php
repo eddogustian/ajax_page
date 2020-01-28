2bec738bc83ac6f58a7a48d92cd7babb42f8d679
Eddo <eddo@nadyne.com>
2020-01-28 16:59:08 +0700

    Master

---
+++ sosmed.php
@@ -0,0 +1,6 @@
+<div class="halaman">
+    <h2>Sosial Media</h2>
+    <p>
+       Facebook: <a href="">
+    </p>
+</div>
---
+++ tentang.php
@@ -0,0 +1,5 @@
+<div class="halaman">
+    <h2>Tentang kami</h2>
+    <p>ini adalah halaman tentang kami</p>
+    <p>DO Dev adalah situs yang menyediakan konten-konten yahuy</p>
+</div>
